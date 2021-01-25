63cda1f958763410b596b14350f3f95db9c7ec4f
anhnd <anhnd@gmail.c>
2021-01-25 16:55:25 +0700

    change config mysql

--- app/CommonBase/DBConnection/ConnectionString.js
+++ app/CommonBase/DBConnection/ConnectionString.js
@@ -4,6 +4,6 @@ if (env === "dev"){
     exports.mysqlConnectionString = "mysql://anhnd9:Vtp@ctbc123@127.0.0.1:3306/QLNV";  //dev
 
 }else{
-    exports.mysqlConnectionString = "mysql://anhnd9:123456789@127.0.0.1:3306/QLNV";//production
+    exports.mysqlConnectionString = "mysql://root:123456789@127.0.0.1:3306/QLNV";//production
 }
 
